3d8517850b26b1bbd4a8f88b5f8d43c0c8e67f36
Anton Gladkov <anton.gladkov@gmail.com>
2017-12-20 10:29:29 +0300

    Allow to return raw data

--- kontur/focus/api.py
+++ kontur/focus/api.py
@@ -20,8 +20,9 @@ class FocusClient(object):
     API_VERSION = 'api3'
     API_PREFIX = 'https://focus-api.kontur.ru'
 
-    def __init__(self, api_key):
+    def __init__(self, api_key, raw=False):
         self.api_key = api_key
+        self.raw = raw
 
     def _query(self, method, **kwargs):
         logger.info('Executing "/%(method)s" with params: %(kwargs)s', {
@@ -36,8 +37,11 @@ class FocusClient(object):
                 data = rp.read()
                 data = data.decode(rp.headers.get_content_charset('UTF-8'))
                 logger.debug('Raw response: %s', data)
-                result = json.loads(data)
-                logger.debug('API JSON response: %s', result)
+                if self.raw:
+                    result = data
+                else:
+                    result = json.loads(data)
+                    logger.debug('API JSON response: %s', result)
         except HTTPError as e:
             logger.exception('HTTP error: %s', str(e))
             raise
